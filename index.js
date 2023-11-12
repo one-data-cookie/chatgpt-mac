89d875bab2d190684f8904ebf4b2d504e7ec9976
one-data-cookie <kolacek.m@gmail.com>
2023-11-12 17:04:20 +0100

    remove analytics

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { menubar } = require("menubar");
-const Nucleus = require("nucleus-analytics");
 
 const path = require("path");
 const {
@@ -19,8 +18,6 @@ const image = nativeImage.createFromPath(
 );
 
 app.on("ready", () => {
-  Nucleus.init("638d9ccf4a5ed2dae43ce122");
-
   const tray = new Tray(image);
 
   const mb = menubar({
